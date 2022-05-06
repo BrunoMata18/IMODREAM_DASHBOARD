b0c0bf7316c2e5a46ab19fca2e69c06d2165d9cf
brunomata18 <brunomiguel660@gmail.com>
2022-05-06 18:14:47 +0100

    PUSH

---
+++ assets/js/connection.js
@@ -0,0 +1,26 @@
+$(document).ready(
+
+    function(){
+        $.ajax({
+            url: "https://imodream-api.herokuapp.com/api/proprety",
+            type: "GET",
+            dataType: 'json',
+            success: function(result) {
+                console.log(result);
+                $('#Moradia_Tipo').text(result)
+                var down = document.getElementById("Moradia_Tipo");
+                var obj = JSON.stringify(result);
+                var res = [""];
+
+                for(var i in obj)
+                    res.push(obj[i]);
+                down.innerHTML = obj[i] ;
+
+                {
+
+                }
+            }
+
+        });
+    }
+);
